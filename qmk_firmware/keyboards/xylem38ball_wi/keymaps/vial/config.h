fe9f4c20733cf890c448d858142eb7dece7cb980
wdknt <44008511+wdknt@users.noreply.github.com>
2025-10-11 17:13:11 +0900

    Update config.h

--- qmk_firmware/keyboards/xylem38ball_wi/keymaps/vial/config.h
+++ qmk_firmware/keyboards/xylem38ball_wi/keymaps/vial/config.h
@@ -6,3 +6,12 @@
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
 #define POINTING_DEVICE_INVERT_X
 #define POINTING_DEVICE_INVERT_Y
+
+// 既存の #define の下に追加
+#define PMW3360_CPI 1200        // トラックボールの解像度
+#define SCROLL_SCALE_NUM 3
+#define SCROLL_SCALE_DEN 8
+
+
+// レイヤー数を6つとする。
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
